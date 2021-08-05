5ba87b0edd770e35173fc6a7dad46d2f9c94de1d
Idan Golshan <idang9@gmail.com>
2021-08-05 14:56:38 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import {Todo} from "./todo";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angular-firstApp';
+  todos: Todo[];
+  constructor () {
+    this.todos = [];
+  }
+
+  //צריך להשלים את הפונקציה שדוחפת טודוס ולהגדיר מי הוא כל טודו
+
+  addTodo() {
+    this.todos.push(todo)
+    //the todo
+  }
+
 }
---
+++ src/app/todo.ts
@@ -0,0 +1,10 @@
+export class Todo {
+  id: number;
+  text: string;
+
+  constructor(text: string) {
+    this.id = 6;
+    this.text = text;
+
+  }
+}
